e4dd727aef2d9f6b6093da209f8b80e122272078
NaelaTarannum <naelatarannum@gmail.com>
2023-11-14 19:34:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.user,
-        pass: process.env.pass
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     },
 });
 
